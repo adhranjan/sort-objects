51f5a2388fa7d4d0b34ec171f530e40e7bdccdbd
Ranjan <ranjan.adhikari@bookmyshow.com>
2022-08-29 20:36:18 +0545

    allow calue type cast while order

--- src/priority-sort.ts
+++ src/priority-sort.ts
@@ -1,31 +1,49 @@
-import * as dot from 'dot-object'
-const compare =  (...args)=> {
-    let properies = args[0]
-    let order = args[1];
-    let first = args[2];
-    let second = args[3];
-    let ret = 1;        
-    for(let i of properies){
-        let a = dot.pick(i, first);
-        let b = dot.pick(i, second);
-        if(a === b){
-            continue;
-        } else if (a > b) {
-            ret = 1;
-        } else {
-            ret = -1;
-        }
-        break;
-    }    
-    if(order === "desc"){
-        ret = -ret;
-    }
-    return ret;
+import * as dot from "dot-object";
+interface CastKeyToDifferentType {
+  key: string;
+  cast: Function;
 }
 
-export const prioritySort = (properties:string[], order?: "asc"| "desc") =>{
-    if(!order){
-        order = "asc";
+const compare = (...args) => {
+  let properies = args[0];
+  let order = args[1];
+  let first = args[2];
+  let second = args[3];
+  let ret = 1;
+  for (let i of properies) {
+    let a, b;
+    if (i.key && i.cast) {
+      a = i.cast(dot.pick(i.key, first));
+      b = i.cast(dot.pick(i.key, second));
+
+      console.log("2");
+    } else {
+      a = dot.pick(i, first);
+      b = dot.pick(i, second);
+      console.log("1");
     }
-    return compare.bind(null,properties,order)
-}
+
+    if (a === b) {
+      continue;
+    } else if (a > b) {
+      ret = 1;
+    } else {
+      ret = -1;
+    }
+    break;
+  }
+  if (order === "desc") {
+    ret = -ret;
+  }
+  return ret;
+};
+
+export const prioritySort = (
+  properties: (CastKeyToDifferentType | string)[],
+  order?: "asc" | "desc"
+) => {
+  if (!order) {
+    order = "asc";
+  }
+  return compare.bind(null, properties, order);
+};
